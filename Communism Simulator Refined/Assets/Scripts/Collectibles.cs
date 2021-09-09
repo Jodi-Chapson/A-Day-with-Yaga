b85a55e752369133b14ad8a20f037b601dc8175e
Jodi Chapson <2152594@students.wits.ac.za>
2021-09-09 15:14:24 +0200

    Added the Manager + Resource Manager
    This is still incomplete - more to come in terms of neatening and preparing for an actual task system :)

---
+++ Communism Simulator Refined/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    [SerializeField]
+    int resourcetype; //0 is wood, 1 is stone
+    ResourceManager rm;
+    Transform player;
+    public bool canCollect;
+
+    [SerializeField]
+    ParticleSystem ps;
+
+    private void Start()
+    {
+        rm = GameObject.Find("Game Manager").GetComponent<ResourceManager>();
+        player = GameObject.Find("Player").transform;
+        ps = GetComponentInChildren<ParticleSystem>();
+        ps.enableEmission = false;
+    }
+
+    private void Update()
+    {
+        if (canCollect)
+        {
+            //Debug.Log(Vector3.Distance(this.transform.position, player.position));
+
+            if (Vector3.Distance(this.transform.position, player.position) < rm.collectionDistance + 0.5)
+            {
+
+                ps.enableEmission = true;
+            }
+            else
+            {
+                ps.enableEmission = false;
+            }
+        }
+    }
+    public void CollectResource()
+    {
+        if (resourcetype == 0)
+        {
+            rm.woodNum++;
+        }
+        else if (resourcetype == 1)
+        {
+            rm.stoneNum++;
+        }
+
+
+
+        Destroy(gameObject);
+    }
+
+
+    private void OnMouseOver()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        if (canCollect)
+        {
+            Debug.Log(Vector3.Distance(this.transform.position, player.position));
+
+            if (Vector3.Distance(this.transform.position, player.position) < rm.collectionDistance)
+            {
+
+                CollectResource();
+            }
+        }
+
+    }
+}
---
+++ Communism Simulator Refined/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int woodNum = 0;
+    public int stoneNum = 0;
+    public int woodmax, stonemax;
+    public Text wood, stone;
+    public float collectionDistance;
+    public GameObject woodstrike, stonestrike;
+    public GameManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        woodNum = 0;
+        stoneNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wood.text = woodNum + "/" + woodmax;
+        stone.text = stoneNum + "/" + stonemax;
+
+        if (woodNum >= woodmax)
+        {
+            if (woodstrike.activeSelf == false)
+            {
+                woodstrike.SetActive(true);
+            }
+        }
+
+
+
+        if (stoneNum >= stonemax)
+        {
+            if (stonestrike.activeSelf == false)
+            {
+                stonestrike.SetActive(true);
+            }
+        }
+
+
+        if (woodstrike.activeSelf == true && stonestrike.activeSelf == true)
+        {
+            //rewrite this spot
+        }
+
+    }
+}
