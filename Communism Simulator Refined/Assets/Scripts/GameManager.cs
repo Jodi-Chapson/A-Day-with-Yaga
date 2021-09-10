452938edc1d2064668ec358a36e9f160c08c5587
2116122-DanielBurke <81285642+2116122-DanielBurke@users.noreply.github.com>
2021-09-10 09:53:38 +0200

    Updated Models, added ignore layer between player and berry mesh

--- Communism Simulator Refined/Assets/Scripts/GameManager.cs
+++ Communism Simulator Refined/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.IgnoreLayerCollision(6, 8, true);
     }
 
     // Update is called once per frame
