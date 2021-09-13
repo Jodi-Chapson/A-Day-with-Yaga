68053a367dcffd40428aea5a83c60ac6dfc3431f
Jodi Chapson <2152594@students.wits.ac.za>
2021-09-13 13:39:27 +0200

    Bdum tsss and music

---
+++ Communism Simulator Refined/Assets/Scripts/Tasks and Dialogue Scripts (coz there will most likely be a fuck ton)/IntroDialogue.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogue : MonoBehaviour
+{
+    [Header("References")]
+    public InteractionManager im;
+    public string[] introdialogue;
+    public int[] speakerID;
+
+    void Start()
+    {
+        im = GameObject.Find("Game Manager").GetComponent<InteractionManager>();
+
+        StartCoroutine(DelayedStartcozfukthisunity());
+
+
+
+    }
+
+
+    public IEnumerator DelayedStartcozfukthisunity()
+    {
+        yield return new WaitForSeconds(0.1f);
+        
+        im.dialogue.Clear();
+        im.speakerID.Clear();
+
+
+
+
+        foreach (int id in speakerID)
+        {
+            im.speakerID.Enqueue(id);
+        }
+
+        foreach (string sentence in introdialogue)
+        {
+            im.dialogue.Enqueue(sentence);
+        }
+
+        im.NextDialogue();
+        im.dialogueActive = true;
+        //im.firsttext = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
