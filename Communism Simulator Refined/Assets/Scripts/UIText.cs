10ba74f3c9c7ff446bf9f649f759e1a7a7369417
Jodi Chapson <2152594@students.wits.ac.za>
2021-09-14 02:36:49 +0200

    QUality of LIfe changes

---
+++ Communism Simulator Refined/Assets/Scripts/UIText.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public bool isText;
+    public Text text;
+    public int size;
+    public Vector3 scale;
+
+    public void Start()
+    {
+        if (isText)
+        {
+            text = this.GetComponentInChildren<Text>();
+            size = text.fontSize;
+        }
+        else
+        {
+            scale = this.GetComponent<RectTransform>().localScale;
+        }
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+        {
+
+        if (isText)
+        {
+            text.fontSize = size + 10;
+        }
+        else
+        {
+            this.GetComponent<RectTransform>().localScale = scale * 1.1f;
+        }
+
+        }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isText)
+        {
+            text.fontSize = size;
+        }
+        else
+        {
+            this.GetComponent<RectTransform>().localScale = scale;
+        }
+
+        //Debug.Log("EXIT");
+        //throw new System.NotImplementedException();
+    }
+
+
+}
